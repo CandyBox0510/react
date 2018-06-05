2306b92a935c57f9e0919501f1bba8d37747250e
candybox0510@naver.com <candybox0510@naver.com>
2018-06-05 16:32:46 +0900

    새로고침해도 유지되도록

--- src/components/Formdata.js
+++ src/components/Formdata.js
@@ -7,7 +7,7 @@ class Formdata extends Component{
     state = {
         uniq : '',
         attr : '',
-        idx : 0,
+        init : true,
         promotionBanner:[
             {
                 'uniq' : 'banner',
@@ -19,7 +19,30 @@ class Formdata extends Component{
             }
         ]
     }
-    
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        // 여기서는 setState 를 하는 것이 아니라
+        // 특정 props 가 바뀔 때 설정하고 설정하고 싶은 state 값을 리턴하는 형태로
+        // 사용됩니다.
+        
+        if(nextProps.subAttr && (prevState.attr==='' && (prevState.init))){
+            return {
+                attr : nextProps.subAttr
+            };
+        }
+        
+        // if( nextProps.onData !== 'default' && nextProps.onData){
+        //     return {
+        //         className: 'promotion-banner '+nextProps.onData
+        //     }
+        // }else{
+        //     return{
+        //         className: 'promotion-banner'
+        //     }
+        // }
+
+        return null; // null 을 리턴하면 따로 업데이트 할 것은 없다라는 의미
+    }
 
     static defaultProps ={
         onAttr : () => console.warn('onAttr not defined'),
@@ -35,16 +58,16 @@ class Formdata extends Component{
         this.setState({
            [e.target.name] : e.target.value,
            uniq : e.target.id,
-           idx : 1
+           init : false
         });
     }
 
+
+
     render(){
         let subAttr = null;
-        if(this.state.idx === 0){
+        if(this.state.init){
             subAttr=this.props.subAttr;
-            console.log(this.state.idx);
-            console.log(subAttr);
         }
         
         return(
